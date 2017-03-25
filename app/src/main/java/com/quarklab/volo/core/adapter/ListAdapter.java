56c006c566e9f26b5fcfac9d20fa9c89d990e646
Rudy <rz.zidan@gmail.com>
2017-03-26 00:50:30 +0200

    Fix leaderboard design

--- app/src/main/java/com/quarklab/volo/core/adapter/ListAdapter.java
+++ app/src/main/java/com/quarklab/volo/core/adapter/ListAdapter.java
@@ -1,6 +1,8 @@
 package com.quarklab.volo.core.adapter;
 
 import android.app.Activity;
+import android.content.res.AssetManager;
+import android.graphics.Typeface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +13,8 @@ import android.widget.TextView;
 import com.facebook.login.widget.ProfilePictureView;
 import com.quarklab.volo.R;
 
+import java.util.Locale;
+
 
 /**
  * Created by rudy on 8/6/16.
@@ -20,6 +24,7 @@ public class ListAdapter extends ArrayAdapter<String> {
     private final String[] players;
     private final String[] scores;
     private final String[] userId;
+    private Typeface typeface;
     public ListAdapter(Activity context,
                       String[] players, String[] userId, String[] scores) {
         super(context, R.layout.list_item, players);
@@ -27,7 +32,7 @@ public class ListAdapter extends ArrayAdapter<String> {
         this.players = players;
         this.userId = userId;
         this.scores = scores;
-
+        this.setFont();
     }
     @Override
     public View getView(int position, View view, ViewGroup parent) {
@@ -39,10 +44,18 @@ public class ListAdapter extends ArrayAdapter<String> {
         ImageView trophy = (ImageView) rowView.findViewById(R.id.trophy);
 
         nameTxt.setText(this.players[position]);
+        nameTxt.setTypeface(this.typeface);
         scoreTxt.setText(this.scores[position]);
+        scoreTxt.setTypeface(this.typeface);
         imageView.setProfileId(this.userId[position]);
         if(position == 0)
             trophy.setImageResource(R.drawable.trophy);
         return rowView;
+    }
+
+    private void setFont(){
+        AssetManager am = this.context.getApplicationContext().getAssets();
+        this.typeface = Typeface.createFromAsset(am,
+                String.format(Locale.US, "fonts/%s", "KBZipaDeeDooDah.ttf"));
     }
 }
