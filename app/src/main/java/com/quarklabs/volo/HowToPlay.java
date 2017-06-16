982c274885c19df66d9a8abf4f439e36da58a79d
rudy <rz.zidan@gmail.com>
2017-06-16 14:39:31 +0200

    Finish Up How to Play screen

--- app/src/main/java/com/quarklabs/volo/HowToPlay.java
+++ app/src/main/java/com/quarklabs/volo/HowToPlay.java
@@ -35,14 +35,7 @@ public class HowToPlay extends FragmentActivity {
 
     @Override
     public void onBackPressed() {
-        if (mViewPager.getCurrentItem() == 0) {
-            // If the user is currently looking at the first step, allow the system to handle the
-            // Back button. This calls finish() on this activity and pops the back stack.
-            super.onBackPressed();
-        } else {
-            // Otherwise, select the previous step.
-            mViewPager.setCurrentItem(mViewPager.getCurrentItem() - 1);
-        }
+        super.onBackPressed();
     }
 
 
